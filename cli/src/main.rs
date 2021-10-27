6fb980c0d16a82c6aa9569ca27c8f72156bb477e
Lukas Krickl <lukas@krickl.dev>
2021-10-27 08:28:32 +0200

    Added better error reporting

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,38 @@
 use logpeek::{Error, Interface};
 
-fn main() -> Result<(), Error> {
+fn handle_error<T>(res: Result<T, Error>) -> Result<T, (exitcode::ExitCode, String)> {
+    match res {
+        Ok(res) => Ok(res),
+        Err(err) => match err {
+            Error::RegexError(err) => Err((exitcode::DATAERR, err.to_string())),
+            Error::IOError(err) => Err((exitcode::IOERR, err.to_string())),
+            Error::SerdeJsonError(err) => Err((exitcode::DATAERR, err.to_string())),
+        },
+    }
+}
+
+fn main() {
     let mut stdout = std::io::stdout();
 
-    let interface = Interface::from_args()?;
-    interface.check(&mut stdout)?;
-    interface.print_json(&mut stdout)?;
-
-    Ok(())
+    let interface = match handle_error(Interface::from_args()) {
+        Ok(interface) => interface,
+        Err(tup) => {
+            println!("{}", tup.1);
+            std::process::exit(tup.0);
+        }
+    };
+    match handle_error(interface.check(&mut stdout)) {
+        Ok(_) => (),
+        Err(tup) => {
+            println!("{}", tup.1);
+            std::process::exit(tup.0);
+        }
+    }
+    match handle_error(interface.print_json(&mut stdout)) {
+        Ok(_) => (),
+        Err(tup) => {
+            println!("{}", tup.1);
+            std::process::exit(tup.0);
+        }
+    }
 }
